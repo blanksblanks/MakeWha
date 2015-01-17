60bf1877ef751ff18125f5447259932c953b752b
I like turtles <hsumelanie@yahoo.com>
2015-01-17 06:13:16 -0500

    array now saves between runs

--- BuyBuyBye/BuyBuyBye/Item.m
+++ BuyBuyBye/BuyBuyBye/Item.m
@@ -10,6 +10,19 @@
 
 @implementation Item
 
-//TODO:
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    self = [super init];
+    if (!self) {
+        return nil;
+    }
+    self.name = [aDecoder decodeObjectForKey:@"name"];
+    self.time = [aDecoder decodeFloatForKey:@"time"];
+    return self;
+}
+
+-(void)encodeWithCoder:(NSCoder *)aCoder {
+    [aCoder encodeObject:self.name forKey:@"name"];
+    [aCoder encodeFloat:self.time forKey:@"time"];
+}
 
 @end
