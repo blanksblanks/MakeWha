8456b4a580bfabfd9e6529ad710bed7232662426
Johnny Chen <j_chen187@yahoo.com>
2015-01-17 02:16:10 -0500

    added new folder.

---
+++ BuyBuyBye/BuyBuyBye/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  BuyBuyBye
+//
+//  Created by Johnny Chen on 1/17/15.
+//  Copyright (c) 2015 JohnnyChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+
+- (IBAction)button:(UIButton *)sender;
+
+
+@end
+
